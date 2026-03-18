ff4df800ce9844f0a22b6a36a325355c53443b67
BioManex <zacharyianharris@gmail.com>
2026-03-18 13:23:36 -0500

    Camera stream

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ from picamera2 import Picamera2
 import io
 import base64
 
-
-
 picam2 = Picamera2()
 camera_config = picam2.create_preview_configuration(main={"size": (640, 480)})
 picam2.configure(camera_config)
