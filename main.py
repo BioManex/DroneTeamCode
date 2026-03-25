f6a8047576b84867f04163125efa8739abdba49f
BioManex <zacharyianharris@gmail.com>
2026-03-25 13:36:23 -0500

    Added in some missing sensor code

--- main.py
+++ main.py
@@ -1,87 +1,101 @@
 # These two modules allow us to run a web server.
-from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask import Flask, render_template # type: ignore
+from flask_socketio import SocketIO # type: ignore
 
 from time import sleep
 # This module lets us pick random numbers.
 import random
 # Add this line to import the BMP180 library:
-from bmp180 import BMP180
+from bmp180 import BMP180 # type: ignore
 
-from picamera2 import Picamera2
+from picamera2 import Picamera2 # type: ignore
 
 import io
 import base64
-import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO # type: ignore
 
-from bmp180 import BMP180
+from bmp180 import BMP180 # type: ignore
 
-from mpu6050 import mpu6050
+from mpu6050 import mpu6050 # type: ignore
 
-import RPi.GPIO as GPIO
+from tfluna import TFLuna # type: ignore
 
+# Camera setup
 picam2 = Picamera2()
 camera_config = picam2.create_preview_configuration(main={"size": (640, 480)})
 picam2.configure(camera_config)
 picam2.start()
-timer=0
-mpu = mpu6050(0x68) # 0x68 is a special value that points to the MPU-6050 on I2C. You don't need to touch this or worry about it.
-# Define our motor controller pins
+
+# Accelerometer setup
+mpu = mpu6050(0x68) 
+
+# Lidar setup
+tfluna = TFLuna() 
+tfluna.open() 
+tfluna.set_samp_rate(5)
+
+# Pressure sensor setup
+bmp = BMP180()
+
+# Motor setup
 IN1 = 12
 IN2 = 13
-ENA = 18 # The new ENA pin
-
-accel_data = mpu.get_accel_data()
-print(accel_data['x'])
-print(accel_data['y'])
-print(accel_data['z'])
-gyro_data = mpu.get_gyro_data()
-print(gyro_data['x'])
-print(gyro_data['y'])
-print(gyro_data['z'])
-
-#bmp = BMP180()
 
 # Here, we create the neccesary base app. You don't need to worry about this.
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-bmp = BMP180()
 # When someone requests the root page from our web server, we return 'index.html'.
 @app.route('/')
 def index():
     return render_template('index.html')
-# ... after the app and socketio lines
+
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-# Create an object to represent our BMP180 sensor
-bmp = BMP180()
 
 # This function runs in the background to transmit data to connected clients.
 def background_thread():
     while True:
         # We sleep here for a single second, but this can be increased or decreased depending on how quickly you want data to be pushed to clients.
         socketio.sleep(1)
+        
+        #Pressure test
         barometricPressure = bmp.get_pressure()
-        # Then, we emit an event called "update_data" - but this can actually be whatever we want - with the data being a dictionary
-        # where 'randomNumber' is set to a random number we choose here. You should replace the data being sent back with your sensor data
-        # that you fetch from things connected to your Pi.
+
+        #Accelerometer test
+        accel_data = mpu.get_accel_data()
+        print(accel_data['x'])
+        print(accel_data['y'])
+        print(accel_data['z'])
+        gyro_data = mpu.get_gyro_data()
+        print(gyro_data['x'])
+        print(gyro_data['y'])
+        print(gyro_data['z'])
+
+        #Lidar test
+        distance, strength, temperature = tfluna.read() 
+        print(f"Distance: {round(distance * 100.0, 2)} cm")
+        print(f"Strength: {strength}")
+        print(f"Temperature: {temperature} C")
+    
         socketio.emit(
             'update_data',
             {
                 'randomNumber': random.randint(1, 100),
                 'barometricPressure': barometricPressure
+                
                 # you can add more here! for instance, something along the lines of:
                 # 'mySensor': mysensor.get_sensor_data(),
             }
         )
-        # To add a your first new sensor, try giving https://docs.aerospacejam.org/getting-started/first-sensor a read!
+        
 @socketio.on('do_a_thing')
 def do_a_thing(msg):
     # 'msg' will contain the data sent from the client, like how we had it before.
     # Let's print what we just received to the console!
     print(msg['hello'])
+
 # This function runs when someone connects to the server - and all we do is start the background thread to update the data.
 @socketio.on('connect')
 def handle_connect():
